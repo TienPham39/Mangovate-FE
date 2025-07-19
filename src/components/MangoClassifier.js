5385ba6ded5110413f783708c4c0daafbc8c68ca
TienPham39 <tienpg39@gmail.com>
2025-07-19 20:02:42 +0700

    Update FE

--- src/components/MangoClassifier.js
+++ src/components/MangoClassifier.js
@@ -70,7 +70,7 @@ const MangoClassifier = () => {
         formData,
         {
           headers: { "Content-Type": "multipart/form-data" },
-          timeout: 15000,
+          timeout: 30000,
         }
       );
 
